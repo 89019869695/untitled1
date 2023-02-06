5dc269ee7afcc2f137879b4520c5033cdb4ef5f0
artemKazanov <artemkazanow990@gmail.com>
2023-02-06 19:02:54 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    /*тут информация про меня
+    где я живу и другая
+     */
+    println("Artem ")   //моё имя
+    println("I live in Rybinsk") //здесь я живу
+println("no experience in IT")
+//print ("year of birth   ")
+print(1983) //я родился
+
+
+}
